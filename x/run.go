899c1cbcf5c2f650183b87797b96797603d5c623
Daniel Pfeifer <daniel@pfeifer-mail.de>
2025-03-19 23:18:07 +0100

    Forward exit code of called command

--- x/run.go
+++ x/run.go
@@ -18,5 +18,12 @@ func Run(cmd *exec.Cmd, verbose bool) error {
 		fmt.Printf("\nExecuting command: %v\n\n", cmd)
 	}
 
-	return cmd.Run()
+	if err := cmd.Run(); err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			os.Exit(exitError.ExitCode())
+		}
+		return err
+	}
+
+	return nil
 }
